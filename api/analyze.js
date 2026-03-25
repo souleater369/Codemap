58b09038fbc22a5d2ef28b1d7b362cdb4926824e
souleater369 <135210740+souleater369@users.noreply.github.com>
2026-03-25 07:54:51 +0530

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
 
    for (let i = 0; i < keys.length; i++) {
      const currentKey = keys[i].trim();
-     const aiUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${currentKey}`;
+     const aiUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${currentKey}`;
 ;
      
      const response = await fetch(aiUrl, {
